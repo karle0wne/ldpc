7fda220c2c6f1fd4a303f7db5b7c1700a5d62510
a.karlov <struf27@yandex.ru>
2018-04-22 13:17:33 +0700

    dev: fix logger
    Signed-off-by: a.karlov <struf27@yandex.ru>

--- src/main/java/ldpc/util/template/TimeLogger.java
+++ src/main/java/ldpc/util/template/TimeLogger.java
@@ -1,29 +1,26 @@
 package ldpc.util.template;
 
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Logger;
 
 public class TimeLogger {
 
     private final long startFunctionTime;
-    private final Logger logger;
     private long divDurationTime;
     private String name;
 
     public TimeLogger(String name, boolean soutStartInfo) {
         this.startFunctionTime = System.currentTimeMillis();
-        this.logger = Logger.getLogger(this.getClass().getName());
         this.divDurationTime = 0;
         this.name = name;
         if (soutStartInfo) {
-            this.logger.info("Начало работы метода: " + name);
+            System.out.println("Начало работы метода: " + name);
         }
     }
 
     public void check() {
         long duration = TimeUnit.MILLISECONDS.toMinutes(System.currentTimeMillis() - startFunctionTime);
         if (duration % 5 == 0 && divDurationTime != duration / 5) {
-            logger.warning("Метод: " + name + ", с момента начала работы функции прошло уже " + duration + " минут!");
+            System.out.println("Метод: " + name + ", с момента начала работы функции прошло уже " + duration + " минут!");
             divDurationTime = duration / 5;
         }
     }
