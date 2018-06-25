1c01ef115de3d491d8cbf67ec11015ad23cdc7cd
a.karlov <struf27@yandex.ru>
2018-06-25 22:42:25 +0700

    dev:fix
    Signed-off-by: a.karlov <struf27@yandex.ru>

--- src/main/java/ldpc/MainApplication.java
+++ src/main/java/ldpc/MainApplication.java
@@ -1,11 +1,12 @@
 package ldpc;
 
 import ldpc.util.service.StandService;
-import ldpc.util.template.LDPCEnums;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.AbstractApplicationContext;
+
+import static ldpc.util.template.LDPCEnums.*;
 
 @Configuration
 @ComponentScan(
@@ -19,7 +20,7 @@ public class MainApplication {
     public static void main(String[] args) {
         AbstractApplicationContext context = new AnnotationConfigApplicationContext(MainApplication.class);
         StandService bean = context.getBean(StandService.class);
-        bean.stand(null, LDPCEnums.TypeOfChannel.AWGN, LDPCEnums.TypeOfDecoding.PRODUCT_SUM);
+        bean.stand(TypeOfCoding.LDPC_GIRTH8_8_4, TypeOfChannel.AWGN, TypeOfDecoding.PRODUCT_SUM);
         context.registerShutdownHook();
     }
 }
